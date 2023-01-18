9f5ba3abc0392885a881cbeebaddd9a0d8f5378d
Josh Howard <JoshHoward.Dev@gmail.com>
2023-01-18 12:32:07 -0500

    Add CSS reset stylesheet.
    Co-authored-by: Chris Oakes <ckoakes1@gmail.com>

--- client/components/MainContainer.tsx
+++ client/components/MainContainer.tsx
@@ -3,7 +3,7 @@ import '../stylesheets/mainContainer.css'
 function MainContainer() {
   return (
     <div className="mainContainer">
-      Header TBD
+      <iframe src="http://localhost:3000/d-solo/HePDRGo4k/node-exporter-nodes?orgId=1&refresh=30s&panelId=5" width="450" height="200"></iframe>
     </div>
   )
 }

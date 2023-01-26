9cae2177ea3ce9678de041286c907ce3dc8092f8
Josh Howard <JoshHoward.Dev@gmail.com>
2023-01-26 11:44:55 -0500

    Add styling to front end on landing page.
    Co-authored-by: Anthony Cho <cho.anthony12@gmail.com>
    Co-authored-by: Brian Jaber <brianjaber@gmail.com>
    Co-authored-by: Chris Oakes <ckoakes1@gmail.com>

--- client/pages/LandingPage.tsx
+++ client/pages/LandingPage.tsx
@@ -1,5 +1,7 @@
 import '../stylesheets/landingPage.css'
+import '../stylesheets/buttons.css'
 import logo from '../assets/logo.png'
+import googleSignIn from '../assets/googleSignIn.png'
 
 function LandingPage() {
 
@@ -19,24 +21,25 @@ function LandingPage() {
         <img id="k8rLogo" src={logo} alt="K8R Logo - A robot butler holds the K8R logo on a silver platter." />
       </div>
       <div className="verticalLine"></div>
-      <div className="loginContainer">
-        <div className="googleOAuthContainer">
-          <a
-            id="googleOAuthButton"
-            href={`/auth/google`}
-          >
-            Login With Google
-          </a>
-        </div>
-        <form id="loginForm" onSubmit={submitLogin}>
-          <input id="usernameInput" name="username" className="loginInput" type="text" placeholder="username" />
-          <input id="passwordInput" name="password" className="loginInput" type="password" placeholder="password" />
-          <button id="loginButton" type="submit">Connect</button>
-        </form>
-        <div className="forgotPasswordContainer">
-          <a href="www.google.com" className="forgotPasswordLink">Forgot your password?</a>
-          <span>-</span>
-          <a href="www.google.com">Question?</a>
+      <div className="rightSideContainer">
+        <div className="loginContainer">
+          <div className="googleOAuthContainer">
+            <a
+              id="googleOAuthButton"
+              href={`/auth/google`}
+            >
+              <img src={googleSignIn} className="googleSignInImg" alt="Google Sign In Button" />
+            </a>
+          </div>
+          <form id="loginForm" onSubmit={submitLogin}>
+            <input id="usernameInput" name="username" className="loginInput" type="text" placeholder="username" />
+            <input id="passwordInput" name="password" className="loginInput" type="password" placeholder="password" />
+            <button id="loginButton" type="submit" className="button-64">Connect</button>
+          </form>
+          <div className="forgotPasswordContainer">
+            <a href="www.google.com" className="forgotPasswordLink">Forgot your password?</a>
+            <a href="www.google.com">Register New Account</a>
+          </div>
         </div>
       </div>
     </div>
